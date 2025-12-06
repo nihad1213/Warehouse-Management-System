9a76a371a131187733508819677d04fd5a102fcd
nihad1213 <nihad.nemetli@gmail.com>
2025-12-06 12:07:14 +0400

    new: column added to users table, roles table created

---
+++ wms-laravel/database/migrations/2025_12_06_075728_add_role_olumn_to_users.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('phone_number')->nullable()->after('email');
+            $table->foreignId('role_id')->constrained('roles')->after('phone_number')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['role_id']);
+            $table->dropColumn('role_id');
+            $table->dropColumn('phone_number');
+        });
+    }
+};
