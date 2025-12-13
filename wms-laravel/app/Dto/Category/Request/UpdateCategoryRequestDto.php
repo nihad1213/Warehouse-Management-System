eb5b387fea19c5f524d72f86d5bda4154117329b
nihad1213 <nihad.nemetli@gmail.com>
2025-12-13 13:05:49 +0400

    new: Update dtos created

---
+++ wms-laravel/app/Dto/Category/Request/UpdateCategoryRequestDto.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Dto\Category\Request;
+
+use Spatie\LaravelData\Attributes\Validation\IntegerType;
+use Spatie\LaravelData\Data;
+use Spatie\LaravelData\Attributes\MapInputName;
+use Spatie\LaravelData\Attributes\Validation\Max;
+use Spatie\LaravelData\Attributes\Validation\Unique;
+use Spatie\LaravelData\Attributes\Validation\Required;
+
+#[MapInputName(SnakeCaseMapper::class)] 
+class UpdateCategoryRequestDto extends Data
+{
+    public function __construct(
+        #[Required, IntegerType]
+        public int $id,
+
+        #[Max(255), Unique('categories', 'name')]
+        public string $name
+    ){}
+}
